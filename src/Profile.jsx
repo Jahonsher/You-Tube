0d9cb5a2808865c3e10c0f1c0d4350f6f1115ec4
Jahonsher <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>
2024-03-28 14:34:32 +0500

    as

---
+++ src/Profile.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+
+
+const Profile = () => {
+  return (
+    <div>
+     <div class="container">
+      <div class="header">
+        <img src="./images/img/menu-logo.svg" alt="rasm"/>
+        <input type="search" name="" id="" placeholder='Search'/>
+        <img class="icon" src="./images/icon/Frame 4.svg" alt="icon" />
+      </div>
+      <div>
+        <section class="left">
+          <div>
+            <img src="./images/icon/home.svg" alt="icon" />
+            <p>Home</p>
+          </div>
+          <div>
+            <img src="./images/icon/trends.svg" alt="icon" />
+            <p>Trends</p>
+          </div>
+          <div>
+            <img src="./images/icon/subs.svg" alt="icon" />
+            <p>Subscriptions</p>
+          </div>
+          <div>
+            <img src="./images/icon/originals.svg" alt="icon" />
+            <p>Originals</p>
+          </div>
+          <hr />
+          <div>
+            <img src="./images/icon/library.svg" alt="icon" />
+            <p>Library</p>
+          </div>
+          <div>
+            <img src="./images/icon/history.svg" alt="icon" />
+            <p>History</p>
+          </div>
+          <div>
+            <img src="./images/icon/your-videos.svg" alt="icon" />
+            <p>Your videos</p>
+          </div>
+          <div>
+            <img src="./images/icon/your-movie.svg" alt="icon" />
+            <p>Your movies</p>
+          </div>
+          <div>
+            <img src="./images/icon/watch-later.svg" alt="icon" />
+            <p>Watch later</p>
+          </div>
+          <div>
+            <img src="./images/icon/show-more.svg" alt="icon" />
+            <p>Show more</p>
+          </div>
+          <hr />
+          <h4>SUBRCRIPTIONS</h4>
+          <div>
+            <img src="./images/icon/sub-photo (1).svg" alt="icon" />
+            <p>Adem Ilter</p>
+          </div>
+          <div>
+            <img src="./images/icon/sub-photo.svg" alt="icon" />
+            <p>Coşkun Aral Anlatıy...</p>
+          </div>
+          
+        </section>
+        <section class="right">
+          <div class="top">
+            <button>All</button>
+            <button>Yatchs</button>
+            <button>Boats</button>
+            <button>RVs</button>
+            <button>Electrical Engineering</button>
+            <button>Photocopier</button>
+            <button>Cookery</button>
+            <button>Apple</button>
+            <button>CSS</button>
+            <button>Figma</button>
+            <button>Astronomy </button>
+            <button>React routers</button>
+            <button>Motorcycles</button>
+            <img src="/images/icon/show-more.svg" alt="rasm" />
+          </div>
+          <div class="bottom">
+            <img src="./images/img/Frame 5.svg" alt="" />
+            <img src="./images/img/Frame 6.svg" alt="" />
+            <img src="./images/img/Frame 5.svg" alt="" />
+
+          </div>
+        </section>
+      </div>
+     </div>
+    </div>
+  )
+}
+
+export default Profile
